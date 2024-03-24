5a8bf58e81882fb6daef3253c135de671b15bb78
uds <udovenko@bluewin.ch>
2024-03-24 18:45:47 +0100

    added simple error handling (via alert) into  evaluate function.

--- src/game_maker/front/dsl.cljs
+++ src/game_maker/front/dsl.cljs
@@ -5,7 +5,7 @@
 ;; Stores references to  babylon engine and scene: {:engine ??, :scene ??, :resize-fn ??}
 (def !babylon (atom nil))
 
-(def ^:private !objects (atom nil))
+(def ^:private !objects (atom {}))
 
 (def ^:private colors {:red     (bb/Color3. 1 0 0)
                        :green   (bb/Color3. 0 1 0)
@@ -30,7 +30,7 @@
   []
   (doseq [obj (vals @!objects)]
     (.dispose obj))
-  (reset! !objects nil)
+  (reset! !objects {})
   (js/console.log "[DEBUG] All scene objects are disposed."))
 
 (defn- replace-object
@@ -39,12 +39,19 @@
   (update objects name (fn [old-obj]
                          (when old-obj (.dispose old-obj))
                          new-obj)))
+
+(defn- get-object
+  "Returns the object with the given name."
+  [name]
+  (if-let [obj (@!objects name)]
+    obj
+    (throw (js/Error. (str "Object with name " name " not found.")))))
   
 
 (defn ^:export create-sphere
   "Create a sphere with a given name at the given position with the given color."
   [name x y z diameter color]
-  (println "-> Creating sphere with" name "name at" x y z "with diameter" diameter "and color" color)
+  (js/console.log "-> Creating sphere with" name "name at" x y z "with diameter" diameter "and color" color)
   (let [sphere (bb/MeshBuilder.CreateSphere "sphere" #js {:diameter diameter} (current-scene))]
     (set! sphere.position (bb/Vector3. x y z))
     (set! sphere.material (bb/StandardMaterial. "sphereMat"))
@@ -55,7 +62,7 @@
 (defn ^:export create-cuboid
   "Create a cuboid with a given name at the given position with the given color."
   [name x y z width height color]
-  (println "-> Creating cuboid with" name "name at" x y z "with color" color)
+  (js/console.log "-> Creating cuboid with" name "name at" x y z "with color" color)
   (let [cuboid (bb/MeshBuilder.CreateBox "cuboid" #js {:width width :height height, } (current-scene))]
     (set! cuboid.position (bb/Vector3. x y z))
     (set! cuboid.material (bb/StandardMaterial. "cuboidMat"))
@@ -66,32 +73,30 @@
 (defn ^:export get-position
   "Returns position of the object with a given name as a vector [x y z]."
   [name]
-  (println "-> Getting position of" name)
-  (when-let [object (@!objects name)]
-    (let [pos object.position]
-      [(.-x pos) (.-y pos) (.-z pos)])))
+  (js/console.log "-> Getting position of" name)
+  (let [pos (.-position (get-object name))]
+    [(.-x pos) (.-y pos) (.-z pos)]))
 
 (defn ^:export set-position
   "Set the position of the  object with a given name to the given position."
   [name x y z]
-  (println "-> Setting position of" name "to" x y z)
-  (when-let [object (@!objects name)]
+  (js/console.log "-> Setting position of" name "to" x y z)
+  (let [object (get-object name)]
     (set! object.position (bb/Vector3. x y z))
     nil))
 
 (defn ^:export set-color 
   "Set the color of the object with a given name to the given color."
   [name color]
-  (println "-> Setting color of" name "to" color)
-  (when-let [object (@!objects name)]
+  (js/console.log "-> Setting color of" name "to" color)
+  (let [object (get-object name)]
     (set! object.material.diffuseColor (get colors color))
     nil))
   
 (defn ^:export dispose
   "Disposes object with a given name."
   [name]
-  (println "-> Disposing " name)
-  (when-let [object (@!objects name)]
-    (.dispose object)
-    nil))
+  (js/console.log "-> Disposing " name)
+  (.dispose (get-object name))
+  nil)
   
--- src/game_maker/front/eval.cljs
+++ src/game_maker/front/eval.cljs
@@ -5,15 +5,20 @@
 
 (def ^:private compile-state (env/default-compiler-env))
 
-(defn evaluate 
+(defn evaluate
   "Used to evaluate generated DSL ClojureScript code in the context of the game-maker.front.dsl namespace."
   [expr]
-  (js/console.log "[DEBUG] Evaluating: " expr)
+  (js/console.log "[DEBUG] Evaluating:" expr)
   (let [opts {:eval    cljs/js-eval
               :load    (partial boot/load compile-state)
               :context :statement
               :ns      'game-maker.front.dsl}]
-    (cljs/eval-str compile-state expr "[expr]" opts println)))
+    (cljs/eval-str compile-state expr "[expr]" opts
+                   (fn [result]
+                     (if-let [error (:error result)]
+                       (do (js/console.error "Evaluation error:" error)
+                           (js/alert (str "Evaluation error: " error)))
+                       (js/console.log "[DEBUG] Evaluation result:" (:value result)))))))
 
 (defn init
   "Initializes the self-hosted Clojure bootstrap environment for the game-maker.front.dsl namespace.
